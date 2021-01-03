29b95a15a49b3c56b3d35e8815ccf3e9765aace9
Julian Galeano Bolivar <jbolivar@tuya.com.co>
2021-01-03 12:32:21 -0500

    se añaden los getter con la anotacion Getter de lombok

--- src/test/java/com/juliangb/framework/pageobjects/SignUpServices.java
+++ src/test/java/com/juliangb/framework/pageobjects/SignUpServices.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.ui.Select;
 public class SignUpServices {
 
     private WebDriver driver;
-    private SignUpPageObject signUpPageObject = new SignUpPageObject(this.driver);
+    private SignUpPageObject signUpPageObject;
 
     SignUpServices signUpServices;
 
@@ -21,63 +21,63 @@ public class SignUpServices {
     }
 
     public void writeFirstName(String firstName) {
-        this.signUpPageObject.g
+        this.signUpPageObject.getFirstNameTextBox().sendKeys(firstName);
 
 
 
     }
 
     public void submitForm(){
-        this.driver.findElement(this.submitButton).click();
+        this.signUpPageObject.getSubmitButton().click();
     }
 
     public void writeLastName(String lastName) {
-        this.driver.findElement(this.lastNameTextBox).sendKeys(lastName);
+        this.signUpPageObject.getLastNameTextBox().sendKeys(lastName);
 
 
     }
 
     public void selectCountry(String country){
-        new Select(this.driver.findElement(this.countryList)).selectByValue(country);
+        new Select(this.signUpPageObject.getCountryList()).selectByValue(country);
 
 
 
     }
     public void selectMaleGender() {
-        this.driver.findElement(this.maleRadioButton).click();
+        this.signUpPageObject.getMaleRadioButton().click();
     }
 
     public void writeEmailAddress(String email) {
-        this.driver.findElement(this.emailAddressTextBox).sendKeys(email);
+        this.signUpPageObject.getLastNameTextBox().sendKeys(email);
 
 
     }
 
     public void writePhone(String phone) {
-        this.driver.findElement(this.phoneTextBox).sendKeys(phone);
+        this.signUpPageObject.getPhoneTextBox().sendKeys(phone);
 
 
     }
     public void selectYear(String year){
-        new Select(this.driver.findElement(dateOfBirthYear)).selectByValue(year);
+        new Select(this.signUpPageObject.getDateOfBirthYear()).selectByValue(year);
     }
 
     public void selectMonth(String month){
-        new Select(this.driver.findElement(dateOfBirthMonth)).selectByValue(month);
+        new Select(this.signUpPageObject.getDateOfBirthMonth()).selectByValue(month);
     }
 
     public void selectDay(String day){
-        new Select(this.driver.findElement(dateOfBirthDay)).selectByValue(day);
+        new Select(this.signUpPageObject.getDateOfBirthDay()).selectByValue(day);
     }
 
     public void writePassword(String password) {
-        this.driver.findElement(this.passwordTextBox).sendKeys(password);
+        this.signUpPageObject.getPasswordTextBox().sendKeys(password);
 
 
     }
 
     public void writeConfirmPassword(String confirmPassword) {
-        this.driver.findElement(this.confirmPasswordTextBox).sendKeys(confirmPassword);
+        this.signUpPageObject.getConfirmPasswordTextBox().sendKeys(confirmPassword);
 
 
     }
